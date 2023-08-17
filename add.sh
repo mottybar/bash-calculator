ab62607268ef3fcfd56dd739699db335f2b8b088
shenhavhershko <shenhav.hershko@gmail.com>
2023-08-17 18:00:23 +0300

    add

---
+++ add.sh
@@ -0,0 +1,9 @@
+add() {
+local result=$(( $1 + $2 ))
+echo "Result: $result"
+is_prime "$result" && echo "Prime number"
+echo "Number is: $(is_odd_even "$result")"
+echo "$(is_div_by_five "$result")"
+}
+
+add $1 $2
